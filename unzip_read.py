d43ffeb6b2b42bdd7ba012507bd8f541646bb954
MatCast <32680270+MatCast@users.noreply.github.com>
2020-10-23 05:31:20 +0200

    Added methods to get values and headders

--- unzip_read.py
+++ unzip_read.py
@@ -13,7 +13,32 @@ def find_all_zips(root, pattern):
     return zips
 
 
-def save_logistics_line(file_str):
+def get_values(line):
+    '''Return a line containing only the values after "=" sign.'''
+    line_list = [col.split('=')[1] for col in line.split(';')[:-1]]
+    line = ';'.join(line_list) + ';'
+    return line
+
+
+def get_headers(line):
+    '''Return a line containing only the values before "=" sign.'''
+    line_list = [col.split('=')[0] for col in line.split(';')[:-1]]
+    line_list.extend(['START_TIME', 'ZIP_PATH'])
+    line = ';'.join(line_list) + ';\n'
+    return line
+
+
+def add_first_file_headers(zip_path):
+    with zipfile.ZipFile(zip_path, 'r') as archive:
+        if archive.filelist:
+            file_str = archive.read(archive.filelist[0]).decode("utf-8")
+            logistics_lines = get_logistics_line(file_str)
+            if logistics_lines:
+                return get_headers(logistics_lines[0])
+
+
+def get_logistics_line(file_str):
+    '''Retrurn all the line after the "LOGISTICS_1" value.'''
     lines = file_str.split('\n')
     to_save = []
     for line in lines:
@@ -24,42 +49,52 @@ def save_logistics_line(file_str):
 
 
 def add_time(filename, line):
+    '''Add Start Time to the line based on the filename.'''
     if line:
-        return line[:-1] + filename + ';'
+        return line + filename.split('.')[0] + ';'
     else:
         return ''
 
 
 def add_time_path(filename, zip_path, line):
+    '''Add Start Time and zip path to the line.'''
     line = add_time(filename, line)
     if line:
-        return line[:-1] + zip_path + '\n'
+        return line + zip_path + '\n'
     else:
         return ''
 
 
-def get_zip_values(zip_file_path):
-    with zipfile.ZipFile(zip_file_path, 'r') as archive:
+def get_zip_values(zip_path):
+    all_lines = []
+    with zipfile.ZipFile(zip_path, 'r') as archive:
         for file in archive.filelist:
             file_str = archive.read(file).decode("utf-8")
-            logistics_lines = save_logistics_line(file_str)
+            logistics_lines = (get_logistics_line(file_str))
             for i, line in enumerate(logistics_lines):
-                logistics_lines[i] = add_time_path(file.filename,
-                                                   zip_file_path, line)
-        return logistics_lines
+                line = get_values(line)
+                print(file.filename, line)
+                logistics_lines[i] = add_time_path(file.filename, zip_path,
+                                                   line)
+            all_lines.extend(logistics_lines)
+        return all_lines
 
 
 def get_all_values_from_zips(root, pattern):
     zips = find_all_zips(root, pattern)
     lines = []
+    headers = ''
     for zip_file in zips:
+        if not headers:
+            headers = add_first_file_headers(zip_file)
+            lines.append(headers)
         lines.extend(get_zip_values(zip_file))
     return lines
 
 
-def lines_to_csv(lines):
+def lines_to_csv(lines, path='logistics_1.csv'):
     to_csv = ''.join(lines)
-    with open('logistics_1.csv', 'w') as file:
+    with open(path, 'w') as file:
         file.write(to_csv)
 
 
@@ -67,3 +102,4 @@ if __name__ == "__main__":
     root = './all_the_zips'
     pattern = "*.zip"
     lines = get_all_values_from_zips(root, pattern)
+    lines_to_csv(lines)
